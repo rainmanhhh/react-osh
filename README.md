1ae72641afef5d60aa7477bcf3de5ca52951a1d6
huangwl <410488219@qq.com>
2020-02-05 11:05:37 +0800

    add babel & external plugin for rollup; add david badge

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # react-osh
-![NPM](https://img.shields.io/npm/l/react-osh)
-![npm](https://img.shields.io/npm/v/react-osh)
 ![CI](https://github.com/rainmanhhh/react-osh/workflows/CI/badge.svg)
 ![Codecov](https://img.shields.io/codecov/c/gh/rainmanhhh/react-osh)
+![npm](https://img.shields.io/npm/v/react-osh)
+![npm bundle size](https://img.shields.io/bundlephobia/minzip/react-osh)
+![David](https://img.shields.io/david/rainmanhhh/react-osh)
+![npm](https://img.shields.io/npm/dm/react-osh)
+![NPM](https://img.shields.io/npm/l/react-osh)
 
 simple react state manager based on observer-util. 'osh' means **O**bservable **S**tate-manager with **H**ooks
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import external from 'rollup-plugin-auto-external'
 import pkg from './package.json'
 
 const globals = {
@@ -15,7 +17,6 @@ export default {
     {file: pkg.main, name: 'main', format: 'umd', sourcemap: true, globals},
     {file: pkg.module, format: 'es', sourcemap: true, globals}
   ],
-  external: ['react'],
   watch: {
     include: 'src/**'
   },
@@ -33,7 +34,10 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    external(),
     // Resolve source maps to the original source
     sourceMaps()
   ]
