ec61c191e33f06cc89382e0bfe38e258c5d5039e
huangwl <410488219@qq.com>
2020-02-06 10:43:17 +0800

    add comments

--- src/computed.ts
+++ src/computed.ts
@@ -2,17 +2,22 @@ import {observe} from '@nx-js/observer-util'
 
 const computedPlaceHolder = {}
 
-export function computed<T>(getter: () => T) {
+/**
+ * wrap value provider to computed value.
+ * a computed value will cache provider compute result and re-compute when depended observable values change
+ * todo fix circle: getA() => getB() => getC() => getA()
+ * @param provider value provider
+ */
+export function computed<T>(provider: () => T) {
   let value = computedPlaceHolder
   // call getter immediately may throw Error when observable object has self-reference, so use placeholder and set lazy to true
-  const reaction = observe(getter, {
+  const reaction = observe(provider, {
     lazy: true,
-    scheduler: (r: typeof getter) => {
+    scheduler: (r: typeof provider) => {
       value = r()
     }
   })
   return function computedValueGetter() {
-    // to fix: circle getA() => getB() => getC() => getA()
     if (value === computedPlaceHolder) value = reaction()
     return value
   }
